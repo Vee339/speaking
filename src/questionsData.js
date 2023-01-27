db8b74c26995814ca037d5a045e6eac0e514da1e
Vee339 <81091817+Vee339@users.noreply.github.com>
2023-01-27 12:22:06 +0530

    More questions added on 27 January 2023

--- src/questionsData.js
+++ src/questionsData.js
@@ -43,5 +43,318 @@ export default [
             "Why did you think it was important to learn it?",
             "How you felt when you learnt it?"
         ]
+    },
+    {
+        heading: "Describe something that surprised you and made you happy",
+        hints: [
+            "What it was?",
+            "Who did this?",
+            "How you found out about it and what you did?",
+            "And explain why it surprised you and made you happy."
+        ]
+    },
+    {
+        heading: "Describe a photo that you took and are proud of",
+        hints: [
+            "When did you take this photo?",
+            "Where you took it?",
+            "What is the photo about?",
+            "Why you feel proud of the photo?"
+        ]
+    },
+    {
+        heading: "Describe a problem you had while shopping online or in a store",
+        hints: [
+            "When it happened?",
+            "What you bought?",
+            "What problem you had?",
+            "And explain how you felt about the experience"
+        ]
+    },
+    {
+        heading: "Describe a time when you saw a lot of plastic waste(e.g. in a park, on the beach, etc.)",
+        hints: [
+            "where and when you saw the plastic waste?",
+            "Why there was a lot of plastic waste?",
+            "What you did after saw it?",
+            "And explain what your thoughts were about this."
+        ]
+    },
+    {
+        heading: "Describe a time when you needed to search for information",
+        hints: [
+            "What information you needed to search for?",
+            "When you searched for it?",
+            "Where you searched for it?",
+            "And explain why you needed to search for it?"
+        ]
+    },
+    {
+        heading: "Describe a person you know who is from different culture",
+        hints: [
+            "Who he/she is?",
+            "Where he/she is from?",
+            "How you knew him/her?",
+            "And explain how you feel about him/her?"
+        ]
+    },
+    {
+        heading: "Describe a time you made a decision to wait for something",
+        hints: [
+            "When it happened?",
+            "What you waited for?",
+            "Why you made the decision?",
+            "And explain how you felt while waiting?"
+        ]
+    },
+    {
+        heading: "Describe a time when you received money as a gift",
+        hints: [
+            "When did you receive it?",
+            "What did you do with it?",
+            "How did you feel about it?"
+        ]
+    },
+    {
+        heading: "Talk about a TV show you like to watch",
+        hints: [
+            "What is the program about?",
+            "When do you watch it?",
+            "Who do you watch it with?",
+            "Why do you like to watch it?"
+        ]
+    },
+    {
+        heading: "Describe a person who you believe dresses well",
+        hints: [
+            "Who this person is?",
+            "What kind of dresses he/she wears?",
+            "How do you know him/her?"
+        ]
+    },
+    {
+        heading: "Describe a person who likes to grow plants",
+        hints: [
+            "Who the person is?",
+            "How do you know the person?",
+            "What types of plants does he/she grows?",
+            "Why does he/she enjoy growing plants?"
+        ]
+    },
+    {
+        heading: "Describe a time you had a disagreement with someone",
+        hints: [
+            "Who was the person?",
+            "Why did you have the disagreement?",
+            "Was the issue resolved? If yes, how?",
+            "How you felt about it?"
+        ]
+    },
+    {
+        heading:"Describe a house/apartment that someone you know lives in.",
+        hints: [
+            "Whose house/apartment this is?",
+            "Where the house/apartment is?",
+            "What it looks like inside?",
+            "and explain what you like or dislike about this person's house/apartment?"
+        ]
+    },
+    {
+        heading: "Describe a place where you go to do outdoor activities or play a sport",
+        hints:[
+            "Where it is located?",
+            "What activities you can do at this place?",
+            "How often do you go there?",
+            "Why do you like this place?"
+        ]
+    },
+    {
+        heading: "Describe a city you would recommend as a nice place to live (not your hometown)",
+        hints: [
+            "What it is?",
+            "Where it is?",
+            "What you know about this place?",
+            "And explain why you recommend it as a nice place?"
+        ]
+    },
+    {
+        heading: "Describe a time when someone asked you for your opinion",
+        hints: [
+            "Who you asked for your opinion?",
+            "Why she/he wanted to know your opinion?",
+            "What opinion you gave?",
+            "And explain how you felt when he/she asked for your opinion?"
+        ]
+    },
+    {
+        heading: "Describe a time when you missed an appointment",
+        hints:[
+            "When and where it happened?",
+            "What the appointment was for?",
+            "What happened when you missed it?",
+            "And explain how you felt about missing the appointment?"
+        ]
+    },
+    {
+        heading: "Describe a time when you were busy",
+        hints: [
+            "When it happened?",
+            "Where you were?",
+            "What you did?",
+            "And explain why you were that busy?"
+        ]
+    },
+    {
+        heading: "Describe a childhood friend",
+        hints: [
+            "Who this person is?",
+            "How you became friends?",
+            "What kind of things you like to do with this person?",
+            "Explain why like him/her?"
+        ]
+    },
+    {
+        heading: "Describe something that you have shared with others (or other person)",
+        hints: [
+            "What you shared?",
+            "When did you shared it?",
+            "Who you shared it with?",
+            "And explain why you shared it or how you felt when you shared it?"
+        ]
+    },
+    {
+        heading: "Describe a story or novel that was particularly interesting to you",
+        hints: [
+            "What story or the novel was?",
+            "Where you came to know about it?",
+            "When you read it?",
+            "Explain how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a movie you would like to watch again",
+        hints: [
+            "When you saw this movie?",
+            "With whom you saw this movie?",
+            "What the movie is about?",
+            "Why do you want to watch this movie again?"
+        ]
+    },
+    {
+        heading: "Describe a time when you were stuck in a traffic jam",
+        hints: [
+            "When and where it happened?",
+            "How long you were in the traffic jam?",
+            "What you did while waiting",
+            "And explain how you felt when you were in that traffic jam?"
+        ]
+    },
+    {
+        heading: "Describe an invention that has changed how people live",
+        hints: [
+            "What it is?",
+            "How it has changed people's lives?",
+            "What benefits did it bring?",
+            "And explain if it is more important for older or younger people?"
+        ]
+    },
+    {
+        heading: "Describe a family member whom you would like to work with in the future",
+        hints: [
+            "Who the person is?",
+            "Have you ever worked with the person before?",
+            "What kinds of work you would like to do with the person",
+            "And explain how you will feel if you can finally work together?"
+        ]
+    },
+    {
+        heading: "Talk about an important river or lake in your country or hometown",
+        hints: [
+            "Which water body it is?",
+            "How do you know about it?",
+            "Why does it impress you so much?"
+        ]
+    },
+    {
+        heading: "Talk about a traditional object of your country or Talk about a traditional product of your country that you bought",
+        hints: [
+            "What is it?",
+            "How is it made?",
+            "When did you try it for the first time?",
+            "Why do you like it?"
+        ]
+    },
+    {
+        heading: "Describe a positive change in your life",
+        hints: [
+            "What the change was?",
+            "When it happened?",
+            "How it happened?",
+            "And explain why it was a positive change?"
+        ]
+    },
+    {
+        heading: "Talk about an important/special event you celebrated",
+        hints: [
+            "What was it?",
+            "When did you celebrate?",
+            "Where did you celebrated?",
+            "Who was with you?",
+            "Why it was special?"
+        ]
+    },
+    {
+        heading: "Describe a time when you observed the stars",
+        hints: [
+            "When you observed the stars?",
+            "Where and with whom you went?",
+            "What you observed?",
+            "And explain how you felt when you saw the stars?"
+        ]
+    },
+    {
+        heading: "Describe a plant, vegetable or that you are familiar with",
+        hints: [
+            "What it is and where it grows?",
+            "How you became familiar with?",
+            "What you like or dislike about it?",
+            "And explain why this is important for your country?"
+        ]
+    },
+    {
+        heading: "Describe a friend you like to talk with",
+        hints: [
+            "Who he/she is?",
+            "What you like to talk about?",
+            "What you like to talk with him/her?",
+            "And explain how you feel about him/her?"
+        ]
+    },
+    {
+        heading: "Describe an intersting neighbour of yours",
+        hints: [
+            "How long you have known this neighbour?",
+            "What sort of person he/she is?",
+            "How often you see him/her?",
+            "And explain what kind of relationship you have with him/her?",
+            "Or Explain why you consider this person to be interesting neighbour?"
+        ]
+    },
+    {
+        heading: "Describe a competition you would like to take part in",
+        hints: [
+            "What kind of competition it is?",
+            "What would you do in this competition?",
+           "Why does this competition interest you?"
+        ]
+    },
+    {
+        heading: "Talk about a special day which was not that expensive or on which you didn't have to spend a lot of money",
+        hints: [
+            "What was the occassion?",
+            "Where you went?",
+            "Who did you celebrate with",
+            "Why it didn't cost you much?"
+        ]
     }
 ]
