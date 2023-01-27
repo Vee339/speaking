971cb2a2094aea0a219be0a790ca9503a57702cb
Vee339 <81091817+Vee339@users.noreply.github.com>
2023-01-27 18:36:52 +0530

    More questions are added on 27 January 2023

--- src/questionsData.js
+++ src/questionsData.js
@@ -356,5 +356,483 @@ export default [
             "Who did you celebrate with",
             "Why it didn't cost you much?"
         ]
+    },
+    {
+        heading: "Describe something that helps you concentrate",
+        hints: [
+            "What it is?",
+            "When you do it?",
+            "How did you learn about it?",
+            "How it helps you concentrate?"
+        ]
+    },
+    {
+        heading: "Describe an activity you do to stay fit",
+        hints: [
+            "What the activity is?",
+            "When and where you usually do it?",
+            "How you do it?",
+            "Explain why it can keep you fit it?"
+        ]
+    },
+    {
+        heading: "Describe something that saves your time",
+        hints: [
+            "What is it?",
+            "When you usually do it?",
+            "Why you do it?",
+            "And explain how you think it saves your time?"
+        ]
+    },
+    {
+        heading: "Describe something important that you lost",
+        hints: [
+            "What it was?",
+            "How you lost it?",
+            "Why was it important to you?",
+            "How you felt when you lost it?"
+        ]
+    },
+    {
+        heading: "Describe an interesting thing you have learned from a foreign culture",
+        hints: [
+            "What it is?",
+            "How you learnt it?",
+            "Why you learnt it?",
+            "And explain how you think it will help others?"
+        ]
+    },
+    {
+        heading: "Describe an occasion when you got positive feedback for work that you did",
+        hints: [
+            "What you did?",
+            "When was it?",
+            "Why you got positive feedback?",
+            "How you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a quiet place you like to spend your time in",
+        hints: [
+            "Where it is?",
+            "How you knew it?",
+            "How often you go there?",
+            "And explain why you like the place?"
+        ]
+    },
+    {
+        heading: "Describe a popular/well-known person in your country",
+        hints: [
+            "Who this person is?",
+            "What he/she has done?",
+            "Why he/she is popular?",
+            "And explain how you feel about this person?"
+        ]
+    },
+    {
+        heading: "Describe a time when you helped a child",
+        hints: [
+            "When it was?",
+            "How you helped him/her?",
+            "Why you helped her/him?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe something that surprised you",
+        hints: [
+            "What it is?",
+            "How you found out about it?",
+            "What you did?",
+            "And explain why it made you happy?"
+        ]
+    },
+    {
+        heading: "Describe an occasion when you lost something",
+        hints: [
+            "What you lost?",
+            "When and where you lost it?",
+            "What you did to find it?",
+            "And explain how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a creative person whose work you admire",
+        hints: [
+            "Who he/she is?",
+            "How do you know him/her?",
+            "What creative things he/she has done?",
+            "And explain why you think he or she is creative?"
+        ]
+    },
+    {
+        heading: "Describe a difficult decision that you once made",
+        hints: [
+            "What the decision was?",
+            "When you made the decision?",
+            "How long did it take you to make the decision?",
+            "And explain why it was a difficult decision to make?"
+        ]
+    },
+    {
+        heading: "Describe something that was broken in your home and then repaired",
+        hints: [
+            "What it is?",
+            "How it was broken?",
+            "How you got it repaired?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe an ambition that you haven't achieved",
+        hints: [
+            "What it is?",
+            "Why you haven't achieved it?",
+            "What you did?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a time when you organized a happy event successfully",
+        hints: [
+            "What the event was?",
+            "How you prepared for it?",
+            "Who helped you to organize it?",
+            "And explain why you think it was a successful event?"
+        ]
+    },
+    {
+        heading: "Describe something you received for free",
+        hints: [
+            "What it was?",
+            "Who you received it from?",
+            "Where you received it?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a famous person that you are interested in",
+        hints: [
+            "Who this person is?",
+            "How you know about this person?",
+            "What sort of life they had before they become famous?",
+            "How this person became famous",
+            "And explain why you like this person?"
+        ]
+    },
+    {
+        heading: "Describe a toy that you liked in your childhood",
+        hints: [
+            "What kind of toy it is?",
+            "When you received it?",
+            "How you played it?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe your experience when you changed your school/college",
+        hints: [
+            "Why did you change your school/college?",
+            "When was it?",
+            "Was that helpful?",
+            "What were the consequences after that?"
+        ]
+    },
+    {
+        heading: "Describe a person you only met once and want to know more about",
+        hints: [
+            "Who he/she is?",
+            "When you met him/her?",
+            "Why do you want to know more about him/her?",
+            "And explain how you feel about him/her?"
+        ]
+    },
+    {
+        heading: "Describe a resolution you made in the new year",
+        hints: [
+            "What it is the resolution?",
+            "How will you complete it?",
+            "How you feel about it?",
+            "Why you made this resolution?"
+        ]
+    },
+    {
+        heading: "Describe a skill that you learned from older people",
+        hints: [
+            "What the skill is?",
+            "Who you learned it from?",
+            "How you learned it?",
+            "And how you feel about it?"
+        ]
+    },
+    {
+        heading: "Describe a person you follow on social media",
+        hints: [
+            "Who he/she is?",
+            "How do you know him/her?",
+            "What he/she posts on social media?",
+            "And explain why you follow him/her on social media?"
+        ]
+    },
+    {
+        heading: "Describe a thing you did to learn another language",
+        hints: [
+            "What language you learned?",
+            "What you did?",
+            "How it helped you learn the language?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a course that impressed you a lot",
+        hints: [
+            "What the course was about?",
+            "Where you took the course?",
+            "What you did during the course?",
+            "And explain why it impressed you a lot?"
+        ]
+    },
+    {
+        heading: "Describe an interesting song",
+        hints: [
+            "What the song is?",
+            "What story the song tells?",
+            "Whether the song is popular?",
+            "And explain why you think it is interesting?"
+        ]
+    },
+    {
+        heading: "Describe a special cake you received from others",
+        hints: [
+            "When it happened?",
+            "Where it happened?",
+            "Who you got the cake from?",
+            "And explain why it's a special cake?"
+        ]
+    },
+    {
+        heading: "Describe an interesting conversation you had with your friend",
+        hints: [
+            "Who you spoke with?",
+            "Where you were?",
+            "What the conversation was about?",
+            "And explain why you think it was interesting?"
+        ]
+    },
+    {
+        heading: "Describe a place in a village that you visited",
+        hints: [
+            "Where it is?",
+            "When you visited this place?",
+            "What you did there?",
+            "And how you feel about this place?"
+        ]
+    },
+    {
+        heading: "Describe a long walk you ever had",
+        hints: [
+            "When this happened?",
+            "Where you walked?",
+            "Who you were with?",
+            "And explain how you felt about this walk?"
+        ]
+    },
+    {
+        heading: "Describe an item of clothing that someone gave you or Describe an important piece of clothing",
+        hints: [
+            "What the clothing was?",
+            "Who gave it to you?",
+            "When you got it?",
+            "And explain why this person gave you the clothing?"
+        ]
+    },
+    {
+        heading: "Describe a city that you think is very interesting",
+        hints: [
+            "Where it is?",
+            "What it is famous for?",
+            "How you knew this city?",
+            "And explain why you think it is very interesting?"
+        ]
+    },
+    {
+        heading: "Describe a rule that you don't like",
+        hints: [
+            "What it is?",
+            "Why you don't like it?",
+            "How others feel about the rule?",
+            "And explain whether you've followed the rule?"
+        ]
+    },
+    {
+        heading: "Describe someone you really like to spend time with ",
+        hints: [
+            "Who this person is?",
+            "How you knew him/her?",
+            "What you usually do together?",
+            "And explain why you like to spend time with him/her?"
+        ]
+    },
+    {
+        heading: "Describe a time you visited a new place",
+        hints: [
+            "Where the new place is?",
+            "When you went there?",
+            "Why you went there?",
+            "And explain how you feel about the place?"
+        ]
+    },
+    {
+        heading: "Describe a house or an apartment you would like to live in",
+        hints: [
+            "What it is like?",
+            "Where it would be?",
+            "Why you would like to live in this house/apartment?",
+            "And how you feel about this house/apartment?"
+        ]
+    },
+    {
+        heading: "Describe a person who contributes to the soceity",
+        hints: [
+            "Who this person is?",
+            "How you knew him/her?",
+            "What type of work he/she does?",
+            "And explain why you think he/she contributes to the soceity?"
+        ]
+    },
+    {
+        heading: "Describe a story someone told you and you remember",
+        hints: [
+            "What the story was about?",
+            "Who told you this story?",
+            "Why you remember it?",
+            "And how you feel about it?"
+        ]
+    },
+    {
+        heading: "Describe a gift you would like to buy for your friend",
+        hints: [
+            "What gift you would like to buy?",
+            "Who you would like to give it to?",
+            "Why you would like to buy a gift for him/her?",
+            "And explain why you choose that gift?"
+        ]
+    },
+    {
+        heading: "Describe a thing you cannot live without (not a mobile or computer)",
+        hints: [
+            "What it is?",
+            "How long have you had it for?",
+            "Why you cannot live without it?",
+            "And explain how you felt when you were without it?"
+        ]
+    },
+    {
+        heading: "Describe a person who impressed you the most when you were in primary school",
+        hints: [
+            "Who he/she is?",
+            "How you knew him/her?",
+            "Why he/she impressed you the most?",
+            "And how you feel about him/her?"
+        ]
+    },
+    {
+        heading: "Describe an item on which you spent more than expected",
+        hints: [
+            "What it is?",
+            "How much did you spend on it?",
+            "Why you bought it?",
+            "And explain why you think you spent more than expected?"
+        ]
+    },
+    {
+        heading: "Describe an activity that you usually do that wastes your time",
+        hints: [
+            "What it is?",
+            "When you usually do it?",
+            "Why do you do it?",
+            "And explain why you think it wastes your time?"
+        ]
+    },
+    {
+        heading: "Describe a skill that was difficult for you to learn",
+        hints: [
+            "When you learned it?",
+            "Why you learned it?",
+            "How you learned it?",
+            "How you felt when you learned?"
+        ]
+    },
+    {
+        heading: "Describe a time you moved to a new home/school",
+        hints: [
+            "When you moved?",
+            "Where you moved?",
+            "Why you moved?",
+            "And how you felt about it?"
+        ]
+    },
+    {
+        heading: "Describe a time when you felt proud of a family member",
+        hints: [
+            "When it happened?",
+            "Who is this person?",
+            "What the person did?",
+            "And explain why you felt proud of him/her?"
+        ]
+    },
+    {
+        heading: "Describe a time you got up early",
+        hints: [
+            "When was it?",
+            "Why did you get up early?",
+            "What did you do after getting up?",
+            "And how did you feel about it?"
+        ]
+    },
+    {
+        heading: "Describe an art or craft activity (e.g. painting, woodwork, etc.) that you had at school",
+        hints: [
+            "What you made?",
+            "How you made it?",
+            "What it looked like?",
+            "And how you felt about this activity?"
+        ]
+    },
+    {
+        heading: "Describe an art or craft activity (e.g. painting, woodwork, etc.) that you had at school",
+        hints: [
+            "What you made?",
+            "How you made it?",
+            "What it looked like?",
+            "And how you felt about this activity?"
+        ]
+    },
+    {
+        heading: "Describe a person who solved a problem in a smart way",
+        hints: [
+            "Who this person is?",
+            "What the problem was?",
+            "How he/she solved it?",
+            "And explain why you think he/she did it in a smart way?"
+        ]
+    },
+    {
+        heading: "Describe an exciting book that you enjoy reading",
+        hints: [
+            "When you read it?",
+            "What kind of book it is?",
+            "What is about?",
+            "And explain why you think it is exciting?"
+        ]
+    },
+    {
+        heading: "Describe a cafe you like to visit",
+        hints: [
+            "Where it is?",
+            "What kinds of food and drinks it serves?",
+            "What do you do there?",
+            "And explain why you like to go there?"
+        ]
     }
 ]
